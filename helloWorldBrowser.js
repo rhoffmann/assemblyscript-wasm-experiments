cf8e71d4809cd8a0e1c76792f09fc75d221b174b
Richard Hoffmann <rhoffmann@fastmail.com>
2020-01-10 15:24:57 +0100

    remove logs

--- helloWorldBrowser.js
+++ helloWorldBrowser.js
@@ -1,10 +1,7 @@
 import { wasmBrowserLoader } from "./wasmBrowserLoader.js";
 
 const runWasmAdd = async () => {
-  console.log("runWasmAdd");
-
   const wasmModule = await wasmBrowserLoader("./build/optimized.wasm");
-  console.log("wasmModule", wasmModule);
 
   const { exports } = wasmModule.instance;
   const result = exports.add(12345, 54321);
--- helloWorldNode.js
+++ helloWorldNode.js
@@ -1,5 +1,3 @@
 const wasmModule = require("./index.js");
-console.log(wasmModule);
 
 const result = wasmModule.add(123, 456);
-console.log(result);
